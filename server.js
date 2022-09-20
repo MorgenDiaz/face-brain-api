73aad41d08e31826a80ebda11f9f68bba009e632
Morgen Diaz <morgen@webslinger.works>
2022-09-20 10:04:11 -1000

    Changed connection config to heroku specified value connectionString.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import { handleImage, handleFaceDetect } from "./controllers/image.js";
 const database = knex({
   client: "pg",
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true,
   },
 });
